168a09054c0eb8c925f46a3a0b64fb29bf90369f
ApexBortz <nicholas@georgetownframeshoppe.com>
2022-05-01 15:06:59 -0700

    N-Bortz JavaScript Clock

--- clock.js
+++ clock.js
@@ -1,4 +1,4 @@
-
+// gets new Date and stores it as currentTime
 const currentTime = new Date()
 
 let currentHours = currentTime.getHours()
@@ -9,14 +9,18 @@ if(currentHours > 12){
 const currentMinutes = currentTime.getMinutes()
 const currentSeconds = currentTime.getSeconds()
 
+// convert the seconds, mins, hours, into degrees
+// values that change throughout the program
 let secondDegree = (360/60) * currentSeconds;
 let minuteDegree = (360/60) * currentMinutes;
 let hourDegree = (360/12) * currentHours;
 
+// DOM selectors - get each hand image by Id 
 const secondHand = document.getElementById('second')
 const minuteHand = document.getElementById('minute')
 const hourHand = document.getElementById('hour')
 
+// program function - setInterval function
 setInterval(function(){
     // interval of increment for each hand
     secondDegree += 6;
@@ -28,10 +32,14 @@ setInterval(function(){
     hourHand.style.transform = "rotate(" + hourDegree + "deg)"
 } ,1000 )
 
+// interval function for the current time
 setInterval(function(){
+    // 
     const currentTime = new Date()
+    //
     const digitalTime = document.getElementById('digitalTime')
+    // display the time text
     digitalTime.innerText = currentTime
 
 },1000)
-
+// ^ refreshes the interval to check the time every second
